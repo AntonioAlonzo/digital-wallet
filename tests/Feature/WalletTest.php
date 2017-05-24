b34eddd860f03d785563365eb9d2338c06dd2590
Roman Ramirez <lordpdc@hotmail.com>
2017-05-24 15:16:08 -0500

    Advance wallet test
    dont work create

--- tests/Feature/WalletTest.php
+++ tests/Feature/WalletTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Wallet;
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -10,28 +11,60 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class WalletTest extends TestCase
 {
-//'name', 'description', 'reportable',
-    use DatabaseTransactions;
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testCreatesWallet()
-    {
-        /*
-        $walletAttributes = [
-            'name' => 'name test',
-            'description'    => 'description test',
-            'reportable' => false,
-        ];
 
-        $walletCreated=Wallet::create($walletAttributes);
-        $this->assertEquals($walletCreated->name,$walletAttributes->name);
-        $this->assertEquals($walletCreated->description,$walletAttributes->description);
-        */
+    public function testWalletGetListSuccess(){
+        $url = '/api/v1/wallets';
+        $statusExpect=200;
+        // Test authenticated access.
+        $response=$this->get($url, $this->headers(User::first()));
 
-
+        $response->assertStatus($statusExpect);
     }
+    public function testWalletGetDetailSuccess(){
+        $url = '/api/v1/wallets/1';
+        $statusExpect=200;
+        // Test authenticated access.
+        $response=$this->get($url, $this->headers(User::first()));
+
+        $response->assertStatus($statusExpect);
+    }
+    public function testWalletCreateSuccess(){
+        $url = '/api/v1/wallets';
+        $statusExpect=200;
+        $wallet=array(
+            'name'=> "prueba",
+        );
+        // Test authenticated access.
+        $response=$this->post($url, $this->headers(User::first()),$wallet);
+
+        $response->assertStatus($statusExpect);
+    }
+    public function testWalletUpdateSuccess(){
+        $url = '/api/v1/categories/1';
+        $statusExpect=200;
+        // Test authenticated access.
+        $response=$this->get($url, $this->headers(User::first()));
+
+        $response->assertStatus($statusExpect);
+    }
+    public function testWalletDeleteSuccess(){
+        $url = '/api/v1/categories/1';
+        $statusExpect=200;
+        // Test authenticated access.
+        $response=$this->get($url, $this->headers(User::first()));
+
+        $response->assertStatus($statusExpect);
+    }
+
+    public function testCategoryListFail(){
+
+        $url = '/api/v1/categories';
+        $statusExpect=400;
+        // Test unauthenticated access.
+        $response=$this->get($url, $this->headers());
+
+        $response->assertStatus($statusExpect);
+    }
+
 
 }
